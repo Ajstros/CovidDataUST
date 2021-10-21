0cce80d0c98129de45ee860a2e838b12d3c6d20e
Ajstros <ajstros@gmail.com>
2021-10-21 00:27:27 -0500

    Fix naming in test file

--- covid_data_test.py
+++ covid_data_test.py
@@ -6,11 +6,11 @@ urls = [
 ]
 dfs = []
 for url in urls:
-    df = covidData.get_data(url)
+    df = covid_data.get_data(url)
     dfs.append(df)
     print(url)
     print(df)
     print()
-    covidData.plot_total_cases(df)
-covidData.plot_total_cases(dfs[0], dfs[1])
-covidData.plot_total_cases_subplots(dfs[0], dfs[1])
+    covid_data.plot_total_cases(df)
+covid_data.plot_total_cases(dfs[0], dfs[1])
+covid_data.plot_total_cases_subplots(dfs[0], dfs[1])
