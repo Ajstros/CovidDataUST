bb98379ee05f99b914279cf07a18377481ce0ffd
Ajstros <ajstros@gmail.com>
2021-10-21 00:21:00 -0500

    Add test file

---
+++ covid_data_test.py
@@ -0,0 +1,16 @@
+import covidData
+
+urls = [
+    'https://www.stthomas.edu/covid19/dashboard/historical/index.html',
+    'https://www.stthomas.edu/covid19/dashboard/',
+]
+dfs = []
+for url in urls:
+    df = covidData.get_data(url)
+    dfs.append(df)
+    print(url)
+    print(df)
+    print()
+    covidData.plot_total_cases(df)
+covidData.plot_total_cases(dfs[0], dfs[1])
+covidData.plot_total_cases_subplots(dfs[0], dfs[1])
